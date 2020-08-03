150faefe945374a498ebc86ece9d2d82f51eeaec
Rafael Escobar <rescobar@redventures.com>
2020-08-03 17:41:17 -0300

    Validate state from Spotify
    Also, remove unwraps.

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ use std::env;
 use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
 use std::path::PathBuf;
 
 use error::{CachePathError, CachePathErrorType};
 
 use rspotify::client::Spotify;
 use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth, TokenInfo};
-use rspotify::util::{generate_random_string, process_token};
+use rspotify::util::generate_random_string;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -24,15 +25,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .cache_path(cache_path)
         .build();
     match get_token(&mut auth).await {
-        Some(token_info) => {
+        Ok(token_info) => {
             let creds = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
             let spotify = Spotify::default().client_credentials_manager(creds).build();
             let me = spotify.me().await;
-            println!("{:?}", me.unwrap());
+            println!("{:?}", me?);
         }
-        None => println!("auth failed"),
+        Err(e) => eprintln!("{}", e),
     };
     Ok(())
 }
@@ -56,51 +57,87 @@ fn get_cache_path() -> Result<PathBuf, CachePathError> {
     }
 }
 
-async fn get_token(auth: &mut SpotifyOAuth) -> Option<TokenInfo> {
+async fn get_token(auth: &mut SpotifyOAuth) -> Result<TokenInfo, Box<dyn Error>> {
     match auth.get_cached_token().await {
-        Some(token_info) => Some(token_info),
-        None => generate_token(auth).await,
+        Some(token_info) => Ok(token_info),
+        None => {
+            let state = generate_random_string(16);
+            let auth_url = auth.get_authorize_url(Some(&state), None);
+            let code_future = get_code_req(auth, state);
+            webbrowser::open(&auth_url)?;
+            return code_future.await;
+        }
     }
 }
 
-async fn generate_token(auth: &mut SpotifyOAuth) -> Option<TokenInfo> {
-    let state = generate_random_string(16);
-    let auth_url = auth.get_authorize_url(Some(&state), None);
-    let code_future = get_code_req(auth);
-    webbrowser::open(&auth_url).unwrap();
-    return code_future.await;
-}
-
-async fn get_code_req(auth: &mut SpotifyOAuth) -> Option<TokenInfo> {
-    let listener = TcpListener::bind("127.0.0.1:29797").unwrap();
-
+async fn get_code_req(
+    auth: &mut SpotifyOAuth,
+    correct_state: String,
+) -> Result<TokenInfo, Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:29797")?;
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+        let mut stream = stream?;
         let mut buffer = [0; 1024];
 
-        stream.read(&mut buffer).unwrap();
+        stream.read(&mut buffer)?;
+        let req = String::from_utf8_lossy(&buffer[..]).to_string();
 
-        let mut req = String::from_utf8_lossy(&buffer[..]).to_string();
-        match process_token(auth, &mut req).await {
-            Some(token_info) => {
-                let response = format!(
-                    "HTTP/1.1 200 OK\r\n\r\n{}",
-                    fs::read_to_string("static/auth-ok.html").unwrap(),
-                );
-                stream.write(response.as_bytes()).unwrap();
-                stream.flush().unwrap();
-                return Some(token_info);
+        let mut code: Option<String> = None;
+        let mut state: Option<String> = None;
+        req.lines().nth(0).map(|header| {
+            header.split("?").nth(1).map(|header| {
+                header.split(" ").nth(0).map(|queries| {
+                    for query in queries.split("&") {
+                        let mut s = query.split("=");
+                        if let (Some(k), Some(v)) = (s.next(), s.next()) {
+                            match k {
+                                "code" => code = Some(v.to_string()),
+                                "state" => state = Some(v.to_string()),
+                                _ => {}
+                            }
+                        }
+                    }
+                })
+            })
+        });
+
+        if let (Some(code), Some(state)) = (code, state) {
+            if state != correct_state {
+                make_http_response(&mut stream, false)?;
+                continue;
             }
-            None => {
-                let response = format!(
-                    "HTTP/1.1 400 BAD REQUEST\r\n\r\n{}",
-                    fs::read_to_string("static/auth-error.html").unwrap(),
-                );
-                stream.write(response.as_bytes()).unwrap();
-                stream.flush().unwrap();
+
+            match auth.get_access_token(&code).await {
+                Some(token_info) => {
+                    make_http_response(&mut stream, true)?;
+                    return Ok(token_info);
+                }
+                None => {
+                    make_http_response(&mut stream, false)?;
+                }
             }
         }
     }
 
-    None
+    // TODO: create error for this
+    Err(Box::new(CachePathError::new(
+        CachePathErrorType::UnkownCachePath,
+    )))
+}
+
+fn make_http_response(stream: &mut TcpStream, ok: bool) -> Result<(), Box<dyn Error>> {
+    let response = if ok {
+        format!(
+            "HTTP/1.1 200 OK\r\n\r\n{}",
+            fs::read_to_string("static/auth-ok.html")?,
+        )
+    } else {
+        format!(
+            "HTTP/1.1 400 BAD REQUEST\r\n\r\n{}",
+            fs::read_to_string("static/auth-error.html")?,
+        )
+    };
+    stream.write(response.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
